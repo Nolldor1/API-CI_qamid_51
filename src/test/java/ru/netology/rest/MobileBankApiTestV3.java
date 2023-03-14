b8098a4f1b744e4e5f6dfafd0f3961587cc8e796
German Gizatullin <snolldors@inbox.ru>
2023-03-15 02:20:51 +0500

    modified tests and schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -10,21 +10,23 @@ import static org.hamcrest.Matchers.*;
 class MobileBankApiTestV3 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200)
-          // специализированные проверки - лучше
-          .contentType(ContentType.JSON)
-          .body("", hasSize(3))
-          .body("[0].currency", equalTo("RUB"))
-          .body("[0].balance", greaterThanOrEqualTo(0))
-      ;
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // специализированные проверки - лучше
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[0].currency", equalTo("RUR"))
+                .body("[1].currency", equalTo("USD"))
+                .body("[2].currency", equalTo("RUB"))
+                .body("[0].balance", greaterThanOrEqualTo(0))
+        ;
     }
 }
